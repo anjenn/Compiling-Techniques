2f7873dd70f622a8278b5d2ec3498e34ddefbb13
anjenn <93240896+anjenn@users.noreply.github.com>
2022-05-10 01:15:47 +0200

    checking error
    invalid symbol - weird

--- RE to DFA/js/main.js
+++ RE to DFA/js/main.js
@@ -6,6 +6,34 @@ const inputF = document.getElementById("inputfile");
 const outputF = document.getElementById("output");
 //symbols treated: .; (; ); +; *; ?; |
 
+// Comparison function
+const compareStr = function (regEx, str) {
+  const RE = infToPostfix(regEx);
+  const convToNFA = toNFA(RE);
+  return search(convToNFA, str);
+};
+
+/* Function that takes the regular expresison, and string
+from the input file and checks the validity
+*/
+
+const inputToNFA = function (arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(
+      `/////////////////\nTesting the Input No.${i}\n/////////////////`
+    );
+    let k = i * 2;
+    arr[k] = arr[k].slice(4);
+
+    result[i] = compareStr(arr[k], arr[k + 1]);
+    return result;
+    //put input to NFA into the above code block
+    //and present the result visually
+    //also try to put the result into output file
+  }
+};
+
 // Reading input file
 inputF.addEventListener("change", function () {
   var file = this.files[0];
@@ -16,27 +44,16 @@ inputF.addEventListener("change", function () {
       console.log(line + " --> " + lines[line]);
       outputF.textContent = reader.result;
     }
+    inputToNFA(lines);
   };
   reader.readAsText(file);
 });
-
-const inputToNFA = function (arr) {
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    result[i] = compareStr(arr[i], arr[i + 1]);
-    //should make it a way that we don't skip it for result array
-    // while i increases by 2
-
-    //put input to NFA into the above code block
-    //and present the result visually
-    //also try to put the result into output file
-  }
-};
 
 // Printing to output file
 
 // test cases - must put this in a separate file, or make it readable from command line
 
+/*
 const RE1 = "(A|b)*|a";
 const RE2 = "324(A+B)*";
 const RE3 = "a*|b";
@@ -48,12 +65,7 @@ const RE7 = "3.24A+B)*";
 const RE8 = "3.24(A+B)*";
 
 console.log("testing");
-
-function compareStr(regEx, str) {
-  const RE = infToPostfix(regEx);
-  const convToNFA = toNFA(RE);
-  return search(convToNFA, str);
-}
-
 console.log(compareStr(RE1, "ab"));
 console.log(compareStr(RE7, "a"));
+
+*/
