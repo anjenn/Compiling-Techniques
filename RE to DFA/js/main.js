6ae658eb64e95420aac5a54111171b957a7701a8
anjenn <93240896+anjenn@users.noreply.github.com>
2022-05-09 22:11:29 +0200

    input
    working

--- RE to DFA/js/main.js
+++ RE to DFA/js/main.js
@@ -42,14 +42,14 @@ ${infToPostfix(RE4)}`);
 inputF.addEventListener("change", function () {
   var file = this.files[0];
   var reader = new FileReader();
-  reader.onload = function () {
-    var fileContentArray = this.result.split(/\r\n|\n/);
-    for (var line = 0; line < fileContentArray.length - 1; line++) {
-      console.log(line + " --> " + fileContentArray[line]);
+  reader.onload = function (progressEvent) {
+    var lines = this.result.split("\n");
+    for (var line = 0; line < lines.length - 1; line++) {
+      console.log(line + " --> " + lines[line]);
+      outputF.textContent = reader.result;
     }
-    outputF.textContent = fileContentArray.result;
   };
-  file.readAsText(this.files[0]);
+  reader.readAsText(file);
 });
 
 console.log("testing");
