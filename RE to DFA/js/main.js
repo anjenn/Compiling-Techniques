359142c447a56dfe42734cf92aa0e11c7bb78fa6
anjenn <93240896+anjenn@users.noreply.github.com>
2022-05-10 21:25:34 +0200

    error found
    when reading line by line
    space is automatically inserted

--- RE to DFA/js/main.js
+++ RE to DFA/js/main.js
@@ -17,6 +17,8 @@ const compareStr = function (regEx, str) {
 from the input file and checks the validity
 */
 
+const RE1 = "(A|b)*|a";
+
 const inputToNFA = function (arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -24,7 +26,19 @@ const inputToNFA = function (arr) {
       `/////////////////\nTesting the Input No.${i}\n/////////////////`
     );
     let k = i * 2;
+    //console.log(arr[0].length);
     arr[k] = arr[k].slice(4);
+
+    console.log(arr[0].length); //9
+    console.log(RE1.length); //8
+
+    /*for (let j = 0; j < arr[0].length; j++) {
+      console.log(arr[0][j]);
+      console.log(RE1[j]);
+    }*/
+
+    // compare arr[0] and RE1 and see why they are different
+    // arr[0] is 1 bit longer than RE1
 
     result[i] = compareStr(arr[k], arr[k + 1]);
     return result;
@@ -44,6 +58,16 @@ inputF.addEventListener("change", function () {
       console.log(line + " --> " + lines[line]);
       outputF.textContent = reader.result;
     }
+
+    /////
+    console.log(lines[0].length);
+    for (let j = 0; j < lines[0].length; j++) {
+      console.log(lines[0][j]);
+      //console.log(RE1[j]);
+    }
+
+    ////
+
     inputToNFA(lines);
   };
   reader.readAsText(file);
@@ -53,8 +77,7 @@ inputF.addEventListener("change", function () {
 
 // test cases - must put this in a separate file, or make it readable from command line
 
-/*
-const RE1 = "(A|b)*|a";
+// const RE1 = "(A|b)*|a";
 const RE2 = "324(A+B)*";
 const RE3 = "a*|b";
 const RE4 = "ba+(b|c)"; //this is suppposed to have concatenation symbols added
@@ -67,5 +90,3 @@ const RE8 = "3.24(A+B)*";
 console.log("testing");
 console.log(compareStr(RE1, "ab"));
 console.log(compareStr(RE7, "a"));
-
-*/
